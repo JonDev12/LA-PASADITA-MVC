eef80e8c80ba58e37ffa2ee6b5e75f06a10fbffb
Jonathan Martinez <jonathan.martinez@bigwavedata.com.mx>
2024-05-07 17:48:26 -0600

    Agrego tareas para Made1311

--- View/Bebidas.php
+++ View/Bebidas.php
@@ -1,2 +1,2 @@
-//TODO Create a new Controller Model for this view
-//TODO use the files on Model and Controllers to create it
+//TODO Create a new Controller Model for this view use the files on Model and Controllers to create it
+//!REMBER Use the ccs named estilos_menu on styles folder
