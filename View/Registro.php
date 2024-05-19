73492089cd735b70ba4f4d3107ce565d1882b569
Madelene Vargas <l21isc013@taamzunchale.tecnm.mx>
2024-05-18 23:17:44 -0600

    modificaciones en el botón del form registro para las validaciones

--- View/Registro.php
+++ View/Registro.php
@@ -12,7 +12,7 @@
 
 <body>
     <div class="form">
-        <form class="form-box" action="../Controller/RegisterUser.php" method="POST">
+        <form class="form-box" action="../Controller/RegisterUser.php" method="POST" onsubmit="return UserValidation()">
             <div>
                 <h1 class="etiquetas">REGISTRARSE</h1>
                 <h1 class="etiquetas">Foto(Opcional)</h1>
@@ -61,9 +61,9 @@
                 </div>
             </div>
             <div class="text-center" style="margin: 1.5rem">
-                <button class="btn btn-primary" type="submit" value="Send">Enviar Datos</button>
-            </div>
+            <button class="btn btn-primary" type="submit" value="Send">Enviar Datos</button>            </div>
         </form>
     </div>
+    <script src="../js_personalizado/UserValidation.js"></script>
 </body>
 </html>
