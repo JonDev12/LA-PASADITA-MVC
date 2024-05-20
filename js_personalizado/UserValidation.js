17d80d23d6ec6c7939903a2dc3427761cfbbe9f2
Jonathan Martinez <jonathan.martinez@bigwavedata.com.mx>
2024-05-20 11:12:59 -0600

    Agrego validación de campos de registro de usuarios

--- js_personalizado/UserValidation.js
+++ js_personalizado/UserValidation.js
@@ -1,8 +1,7 @@
-class  UserValidation{
-    
-    constructor(name, ap1, ap2, type, phone, user, passwd){
+class UserValidation {
+    constructor(name, ap1, ap2, type, phone, user, passwd) {
         this.name = name;
-        this.ap1  = ap1;
+        this.ap1 = ap1;
         this.ap2 = ap2;
         this.type = type;
         this.phone = phone;
@@ -11,15 +10,15 @@ class  UserValidation{
     }
 
     ValidateUserEntry() {
-        this.name = document.getElementsByName('txt_name');
-        this.ap1 = document.getElementsByName('txt_lname1');
-        this.ap2 = document.getElementsByName('txt_lname2');
-        this.type = document.getElementsByName('cbx_type_user');
-        this.phone = document.getElementsByName('txt_phone');
-        this.user = document.getElementsByName('txt_username');
-        this.passwd = document.getElementsByName('txt pwd');
+        this.name = document.getElementsByName('txt_name')[0].value;
+        this.ap1 = document.getElementsByName('txt_lname1')[0].value;
+        this.ap2 = document.getElementsByName('txt_lname2')[0].value;
+        this.type = document.getElementsByName('cbx_type_user')[0].value;
+        this.phone = document.getElementsByName('txt_phone')[0].value;
+        this.user = document.getElementsByName('txt_username')[0].value;
+        this.passwd = document.getElementsByName('txt_pwd')[0].value;
 
-        if(this.name == "" || this.ap1 == "" || this.ap2 == "" || this.type == "" || this.phone == "" || this.user == "" || this.passwd == ""){
+        if (this.name === "" || this.ap1 === "" || this.ap2 === "" || this.type === "......." || this.phone === "" || this.user === "" || this.passwd === "") {
             alert("Todos los campos son requeridos");
             return false;
         }
@@ -27,21 +26,19 @@ class  UserValidation{
         return this.IsRequiredPwd();
     }
 
-    //TODO: Complete this method to validate passwd field 
-    IsRequiredPwd(){
+    IsRequiredPwd() {
         if (this.passwd.length < 8) {
             alert("La contraseña debe tener al menos 8 caracteres");
             return false;
         } else if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/.test(this.passwd)) {
             alert("La contraseña debe contener al menos una letra y un número");
             return false;
-        } else {
-            alert('La contraseña no es valida, intente otra vez');
-            return true;
         }
+        return true;
     }
 }
-const ValidatePwd = new UserValidation();
+
+const validatePwd = new UserValidation();
 document.getElementById('Registro').addEventListener('submit', function(event) {
     if (!validatePwd.ValidateUserEntry()) {
         event.preventDefault(); // Prevenir el envío del formulario si la validación falla
